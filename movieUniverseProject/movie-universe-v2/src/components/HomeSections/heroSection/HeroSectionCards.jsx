95f6011772e0385bb9724778d50eaab24c563481
aakhmakhkout <aakhmakhkout0099@gmail.com>
2026-07-31 12:46:00 +0530

    updated herosectioncards ui, added titile, release date, overview, rating, votes etc

--- movieUniverseProject/movie-universe-v2/src/components/HomeSections/heroSection/HeroSectionCards.jsx
+++ movieUniverseProject/movie-universe-v2/src/components/HomeSections/heroSection/HeroSectionCards.jsx
@@ -1,21 +1,18 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
 import { getBackdrop } from "@/lib/tmdb/images";
-
 export default function HeroSectionCards({ moviesList }) {
   const { trending_movies } = moviesList;
   console.log(trending_movies);
   const [increment, setincrement] = useState(0);
   const imageSrc = getBackdrop(trending_movies[increment].backdrop_path);
-  console.log(imageSrc);
   return (
     <div>
       <div
-        className={`w-[98%] h-200 relative bg-cover bg-center flex flex-col items-center rounded-xl`}
+        className={`w-[98%] h-160 relative bg-cover bg-center flex flex-col items-center rounded-xl`}
         style={{
-          background: `linear-gradient(90deg,rgba(0, 0, 0, 0.58) 38%, rgba(255, 255, 255, 0) 60%, rgba(255, 255, 255, 0) 86%), url(${imageSrc})`,
+          background: `linear-gradient(90deg,rgba(0, 0, 0, 0.83) 26%, rgba(255, 255, 255, 0) 54%, rgba(255, 255, 255, 0) 86%), url(${imageSrc})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -26,11 +23,21 @@ export default function HeroSectionCards({ moviesList }) {
             Trending Movies
           </h1>
         </div>
-        <div>
-          <h1>name</h1>
-          <div>
-            <p>release date</p>
+        <div className="flex flex-col bg-black/20 w-full gap-5">
+          <h1 className="text-7xl font-black text-white drop-shadow-[0_4px_12px_rgba(0,0,0,0.8)]">
+            {trending_movies[increment].original_title}
+          </h1>
+          <div className="flex">
+            <p>{trending_movies[increment].release_date}</p>
             <p>genre</p>
+          </div>
+          <div>
+            <p>{trending_movies[increment].overview}</p>
+          </div>
+          <div>
+            <p>{trending_movies[increment].vote_average}</p>
+            <p>IMDB</p>
+            <p>{trending_movies[increment].vote_count}</p>
           </div>
         </div>
       </div>
